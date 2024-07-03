b78e933b7b54ee785c6f7f6b67a1a28980d370a0
Hussainbhanpura <108626978+Hussainbhanpura@users.noreply.github.com>
2024-07-04 00:08:19 +0530

    backend origin

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,11 @@ const Purchase = require('./modal/purchaseModal');
 const app = express();
 const port = process.env.PORT || 3001;
 const mongoUrl = process.env.MONGO_URL;
-// app.use(cors({
-//     orgin : [""],
-//     methods : ["POST", "PUT", "DELETE", "PATCH","GET"],
-//     credentials: true, 
-// }));
+app.use(cors({
+    orgin : ["https://accounts-flax.vercel.app/"],
+    methods : ["POST", "PUT", "DELETE", "PATCH","GET"],
+    credentials: true, 
+}));
 app.use(cors())
 
 // MongoDB connection
