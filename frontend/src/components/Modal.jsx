25019e862a75c511404db07cb9db03d8e0c74ddc
Hussainbhanpura <108626978+Hussainbhanpura@users.noreply.github.com>
2024-07-05 15:59:48 +0530

    not sending post twice

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -22,14 +22,8 @@ const Modal = ({ modal, setTotal, allTransactions }) => {
       .catch((error) => {
         console.error("Error adding purchase:", error);
       });
-    // window.refresh();
-
-    axios.post(`${BASE_URL}/data`, {
-      inwardsAmount,
-      purchaseAmount,
-      gst,
-    });
     modal(false);
+    // window.refresh();
   };
 
   const handleInputChange = (e, setter) => {
@@ -41,83 +35,83 @@ const Modal = ({ modal, setTotal, allTransactions }) => {
   }, [purchaseAmount, gstRate]);
   return (
     <div
-      className="modal fade show"
-      tabIndex="-1"
-      role="dialog"
+      className='modal fade show'
+      tabIndex='-1'
+      role='dialog'
       style={{ display: "block" }}
     >
-      <div className="modal-dialog" role="document">
-        <div className="modal-content">
-          <div className="modal-header">
+      <div className='modal-dialog' role='document'>
+        <div className='modal-content'>
+          <div className='modal-header'>
             <button
-              type="button"
-              className="close"
-              data-dismiss="modal"
-              aria-label="Close"
+              type='button'
+              className='close'
+              data-dismiss='modal'
+              aria-label='Close'
             >
-              <span aria-hidden="true">&times;</span>
+              <span aria-hidden='true'>&times;</span>
             </button>
-            <h5 className="modal-title">Add Purchase</h5>
+            <h5 className='modal-title'>Add Purchase</h5>
           </div>
 
-          <div className="container mt-5">
+          <div className='container mt-5'>
             <form onSubmit={handleSubmit}>
-              <div className="form-group">
-                <label htmlFor="inwards">Inwards</label>
+              <div className='form-group'>
+                <label htmlFor='inwards'>Inwards</label>
                 <input
                   value={inwardsAmount}
                   onChange={(e) => handleInputChange(e, setinwards)}
-                  id="inwards"
-                  type="number"
-                  className="form-control"
+                  id='inwards'
+                  type='number'
+                  className='form-control'
                   style={{ width: "50%" }}
                 />
               </div>
-              <div className="form-group">
-                <label htmlFor="purchase">Purchase</label>
+              <div className='form-group'>
+                <label htmlFor='purchase'>Purchase</label>
                 <input
                   onChange={(e) => handleInputChange(e, setpurchase)}
                   value={purchaseAmount}
-                  id="purchase"
-                  type="number"
-                  className="form-control"
+                  id='purchase'
+                  type='number'
+                  className='form-control'
                   style={{ width: "50%" }}
                 />
               </div>
-              <div className="form-group">
-                <label htmlFor="gst">GST Rate</label>
+              <div className='form-group'>
+                <label htmlFor='gst'>GST Rate</label>
                 <input
                   onChange={(e) => handleInputChange(e, setgstRate)}
                   value={gstRate}
-                  id="gst"
-                  type="number"
-                  className="form-control"
+                  id='gst'
+                  type='number'
+                  className='form-control'
                   style={{ width: "50%" }}
                 />
               </div>
-              <div className="form-group">
-                <label htmlFor="gst">GST</label>
+              <div className='form-group'>
+                <label htmlFor='gst'>GST</label>
                 <input
                   onChange={(e) => handleInputChange(e, setgst)}
                   value={gst}
-                  id="gst"
-                  type="number"
-                  className="form-control"
+                  id='gst'
+                  type='number'
+                  className='form-control'
                   style={{ width: "50%" }}
                 />
               </div>
-              <div className="text-center"></div>
+              <div className='text-center'></div>
 
-              <div className="modal-footer">
+              <div className='modal-footer'>
                 <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-dismiss="modal"
+                  type='button'
+                  className='btn btn-secondary'
+                  data-dismiss='modal'
                   onClick={() => modal(false)}
                 >
                   Close
                 </button>
-                <button type="submit" className="btn btn-primary">
+                <button type='submit' className='btn btn-primary'>
                   Add Purchase
                 </button>
               </div>
